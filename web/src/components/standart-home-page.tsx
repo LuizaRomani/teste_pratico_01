d777b762eed2bab685ffbc6a4e34c655529706ee
Luiza Sofal <lrsofal@sga.pucminas.br>
2025-05-16 16:55:14 -0300

    finish front

--- web/src/components/standart-home-page.tsx
+++ web/src/components/standart-home-page.tsx
@@ -5,7 +5,7 @@ import { MyLinksCard } from "./my-links-card";
 export function StandartHomePage() {
 	return (
 		<div className="min-h-screen bg-gray-200 flex flex-col items-center">
-			<div className="w-full max-w-[1200px] px-4 sm:px-6 lg:px-8">
+			<div className="w-full max-w-[1200px] px-4 sm:px-6 lg:px-12 space-y-7 mt-32">
 				<LogoHeader />
 				<div className="flex flex-col lg:flex-row items-start gap-8 w-full mt-16 lg:mt-36">
 					<NewLinkCard />
